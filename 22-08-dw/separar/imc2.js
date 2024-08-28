cdbed4d7773c7aec86867d1dd567daf501c45053
isaturquino <163414038+isaturquino@users.noreply.github.com>
2024-08-28 20:19:39 -0300

    Update imc2.js

--- 22-08-dw/separar/imc2.js
+++ 22-08-dw/separar/imc2.js
@@ -2,7 +2,7 @@
 // Captura os elementos html que serão manipulados pelo javscript
 let inputPeso = document.getElementById("peso")
 let inputAltura = document.getElementById("altura")
-let divSaída = document.getElementById("saida")
+let divSaida = document.getElementById("saida")
 
 let botaoCalcular = document.getElementById("calcular")
 botaoCalcular.onclick = calculaImc
@@ -12,10 +12,10 @@ function calculaImc (){
     let peso = Number(inputPeso.value)
     let altura = Number(inputAltura.value)
     // PROCESSO
-    let imc = (peso) / (altura * altura)
+    let imc = peso / (altura * altura)
 
     // SAÍDA
-    divSaída.innerText = imc
+    divSaida.innerText = imc
 
 }
 
