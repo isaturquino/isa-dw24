5cfe37fcc0808e886157e85a6637ab5f1f17d4e6
isaturquino <163414038+isaturquino@users.noreply.github.com>
2024-09-05 13:48:00 -0300

    Update multa.js

--- 05-09-dw/multa.js
+++ 05-09-dw/multa.js
@@ -13,20 +13,23 @@ function calculaMulta (){
     
     // calcula o excesso
     let excesso = veiculoVelocidade - velocidadeMax
+    // calcula o quantos % de excesso
+    let porcentagemExcesso = (excesso / velocidadeMax) * 100;
+
 
     // calcula a multa 
     if (excesso <= 0 ){
-        mensagem = "Não tem multa";
+        mensagem = "Não tem multa.";
     }
     else if (excesso <= (velocidadeMax * 0.20) ){
-        mensagem = "Multa de R$ 130,16 - Até 20% acima do limite permitido."; 
+        mensagem = "Multa de R$ 130,16 - Você excedeu " + porcentagemExcesso.toFixed(2)+ "% do limite da via."; 
     }
     else if (excesso <= (velocidadeMax * 0.50) ){
         mensagem = "Multa de R$ 195,23 - De 20% até 50% acima do limite permitido";
     }
-    else excesso > (velocidadeMax * 0.50){
+    else {
         mensagem = "Multa de R$ 880,41 - - Acima de 50% do limite permitido";
     }
 
-    divsaida.innerText = mensagem;
+    divsaida.innerText = mensagem
 }
