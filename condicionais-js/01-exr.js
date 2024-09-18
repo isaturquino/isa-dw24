6862feee896ce7d5dee5768f750b62b3a4bde169
isaturquino <163414038+isaturquino@users.noreply.github.com>
2024-09-17 22:18:05 -0300

    atividade de condicionais js

---
+++ condicionais-js/01-exr.js
@@ -0,0 +1,24 @@
+let condicao1 = false
+let condicao2 = true
+
+console.log("1")
+console.log("2")
+
+if (condicao1 == true){
+    if (condicao3) {
+        console.log("A")
+    } else {
+        console.log ("B")
+    }
+
+} else {
+    
+    if (condicao2) {
+        console.log("C");
+    } else {
+        console.log("D");
+    }
+}
+
+console.log("3")
+console.log("4")
