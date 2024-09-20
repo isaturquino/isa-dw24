688a980405c3bc49334e4f6656fea43cb4aecbd5
isaturquino <163414038+isaturquino@users.noreply.github.com>
2024-09-20 15:31:48 -0300

    Create ex-02.js

---
+++ ex-selecao-js/ex-02.js
@@ -0,0 +1,24 @@
+
+function ehTriangulo(lado1, lado2, lado3){
+    // desenvolver a função aqui
+    if(l1 < l2+l3 && l2-l3){
+    } else if (l1 < l2+l3 && l3-l2){
+    } else if (l2 <l1+l3 && l1-l3){
+    } else if (l2<l1+l3 && l3-l1){
+    } else if (l3< )
+}
+
+
+// PROGRAMA PRINCIPAL
+
+let l1 = 10
+let l2 = 20
+let l3 = 15
+
+if (ehTriangulo(l1,l2,l3)){
+    console.log(`Os lados ${l1}, ${l2} e ${l3} formam um triângulo`)
+}else {
+    console.log(`Os lados ${l1}, ${l2} e ${l3} NÃO formam um triângulo`)
+}
+
+// -------------------------------------------
