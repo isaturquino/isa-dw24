c0d7f3b10e37fcd00fa46f0b9f446a5968d45d76
isaturquino <163414038+isaturquino@users.noreply.github.com>
2024-11-12 20:44:57 -0300

    Update ex-09.js

---
+++ arrays-06-11/ex-09.js
@@ -0,0 +1,12 @@
+// exercício 8 
+function reverseArray(arr) {
+    let reversedArray = [];
+    for (let element of arr) {
+        reversedArray.unshift(element);
+    }
+
+    return reversedArray;
+}
+
+console.log(reverseArray([1, 2, 3, 4]));// Deve exibir: [4, 3, 2, 1]
+console.log(reverseArray(["apple", "banana"])) // Deve exibir: ["banana", "apple"]
