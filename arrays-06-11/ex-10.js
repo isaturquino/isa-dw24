094664886a6ff9fa312a5eea2c145b033d873ced
isaturquino <163414038+isaturquino@users.noreply.github.com>
2024-11-12 20:49:54 -0300

    Update ex-10.js

---
+++ arrays-06-11/ex-10.js
@@ -0,0 +1,7 @@
+// exercício 10
+function findIndex(arr, element){
+
+}
+
+console.log(findIndex([1, 2, 3, 4], 3)); // Deve exibir: 2
+console.log(findIndex(["apple", "banana"], "cherry")); // Deve exibir: -1
