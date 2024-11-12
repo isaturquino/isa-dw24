83a55df7b4dd3c23b65a194233fb208de4118dbc
isaturquino <163414038+isaturquino@users.noreply.github.com>
2024-11-12 20:38:07 -0300

    Update ex-06.js

--- arrays-06-11/ex-06.js
+++ arrays-06-11/ex-06.js
@@ -1,16 +1,14 @@
-//  Encontrar a Média dos Elementos
-
 function average(array){
     let soma = 0 
     let media = 0
     for (elemento of array){
         soma += elemento
-        media = soma % array.lenght
+        media = soma / array.length
     }
     return media
 
-
 }
 
-console.log(average([1, 2, 3, 4])); // Deve exibir: 2.5
-console.log(average([10, 20, 30])); // Deve exibir: 20
+console.log(average([1, 2, 3, 4])) // Deve exibir: 2.5
+console.log(average([10, 20, 30])) // Deve exibir: 20
+console.log(average([15,16,17])) // Deve exibir: 16
