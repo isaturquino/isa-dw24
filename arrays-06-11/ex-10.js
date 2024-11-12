71e9cf6b21163bc25ba380850368dcaeee808c8b
isaturquino <163414038+isaturquino@users.noreply.github.com>
2024-11-12 20:52:07 -0300

    Update ex-10.js

--- arrays-06-11/ex-10.js
+++ arrays-06-11/ex-10.js
@@ -1,6 +1,11 @@
 // exercício 10
 function findIndex(arr, element){
-
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === element) {
+            return i
+        }
+    }
+    return -1
 }
 
 console.log(findIndex([1, 2, 3, 4], 3)); // Deve exibir: 2
