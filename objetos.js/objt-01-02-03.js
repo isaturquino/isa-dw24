5c7e6d0531f31e7efa82a215b9630dbc28e2ef86
isaturquino <163414038+isaturquino@users.noreply.github.com>
2024-11-19 07:44:01 -0300

    Update objt-01-02-03.js

--- objetos.js/objt-01-02-03.js
+++ objetos.js/objt-01-02-03.js
@@ -1,7 +1,15 @@
-// Criando um Objeto Simples
-const carro1 = {
+// Exercício 01
+const carro = {
     marca: "toyota",
     modelo: "RAV4",
     ano:2014,
 }
-console.log(carro1)
+// EXERCÍCIO 02
+console.log(carro.marca)
+carro.ano = 2021
+console.log(carro.ano)
+
+// exercício 03
+carro.cor = "preto"
+delete carro.modelo
+console.log(carro)
