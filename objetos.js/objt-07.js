effaa036118388f201a7b07eb7b6545c72d943ed
isaturquino <163414038+isaturquino@users.noreply.github.com>
2024-11-19 08:20:16 -0300

    Update objt-07.js

---
+++ objetos.js/objt-07.js
@@ -0,0 +1,16 @@
+const carrinho = {
+    itens: [
+        { nome: "Melância", preco: 18.50 },
+        { nome: "Abacaxi", preco: 6.90 },
+        { nome: "Melão", preco: 12.90 }
+    ],
+    calcularTotal: function() {
+        let total = 0; 
+        for (let element of this.itens) {
+            total += element.preco; 
+        }
+        return total;
+    }
+};
+
+console.log("Total do carrinho: R$ " + carrinho.calcularTotal());
